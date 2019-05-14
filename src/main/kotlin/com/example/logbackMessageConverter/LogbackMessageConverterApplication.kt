556982875d31918cf0dfd4510afab9399f2239a7
Cnfn <cnfnss@gmail.com>
2019-05-14 22:03:51 +0800

    init

---
+++ src/main/kotlin/com/example/logbackMessageConverter/LogbackMessageConverterApplication.kt
@@ -0,0 +1,11 @@
+package com.example.logbackMessageConverter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogbackMessageConverterApplication
+
+fun main(args: Array<String>) {
+	runApplication<LogbackMessageConverterApplication>(*args)
+}
